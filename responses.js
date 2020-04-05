4027aa61b08292b5ba218002fa72025c8aa370c1
Edward Leoni <mail@edwardleoni.com>
2020-04-06 10:12:23 +1200

    Remove films and add help response

--- responses.js
+++ responses.js
@@ -2,7 +2,7 @@ module.exports = {
     en: {
         translation: {
             WELCOME_MSG: `Welcome, you can ask The Empire about planets, people. and species. You can ask for help or you can start learning now about the people, species. and planets of the galaxy`,
-            HELP_MSG: `You can say things such as?`,
+            HELP_MSG: `You can find details about anyone in the Galaxy. For example: Ask the empire to find person named Luke Skywalker. <break time="1s"/> You can find details about the different species that inhabit the galaxy. For example: Ask the empire to find details about the Hutt species. <break time="1s"/> Lastly, you can also find planetary information. For example: Ask the empire to find data for Tatooine`,
             GOODBYE_MSG: `Goodbye!`,
             REFLECTOR_MSG: `You just triggered {{intentName}}`,
             ERROR_MSG: `Sorry, I couldn't understand what you said. Can you reformulate?`
