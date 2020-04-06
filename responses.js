9056d57e1b460b08ceec533dff9c2dc55e39340f
Edward Leoni <mail@edwardleoni.com>
2020-04-06 19:49:15 +1200

    Remove yoda laughter

--- responses.js
+++ responses.js
@@ -5,7 +5,7 @@ module.exports = {
             HELP_MSG: `You can find details about anyone in the Galaxy. For example: Ask the empire to find person named Luke Skywalker. <break time="1s"/> You can find details about the different species that inhabit the galaxy. For example: Ask the empire to find details about the Hutt species. <break time="1s"/> Lastly, you can also find planetary information. For example: Ask the empire to find data for Tatooine`,
             GOODBYE_MSG: `May the force be with you!`,
             REFLECTOR_MSG: `You just triggered {{intentName}}`,
-            ERROR_MSG: `Understand you I could not. Please repeat. Or ask for help you could try  Herh herh herh.`,
+            ERROR_MSG: `Understand you I could not. Please repeat. Or ask for help you could try.`,
             PERSON_MSG: `{{name}} is {{gender}}, {{height}} centimetres tall, and weighs {{weight}} kilos. Of {{skinColour}} skin, {{eyeColour}} eyes, and a {{hairColour}} hair`,
             SPECIES_MSG: `The {{name}} have an average height of {{height}} metres, live for {{lifespan}} years in average, are of the {{classification}} family, and speak {{language}}`,
         }
