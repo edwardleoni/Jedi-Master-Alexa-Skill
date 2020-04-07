6b252b54cd4f3fe0981a0078e8b324ef0d86a486
Edward Leoni <mail@edwardleoni.com>
2020-04-08 06:21:15 +1200

    Remove references from old skill name

--- responses.js
+++ responses.js
@@ -1,7 +1,7 @@
 module.exports = {
     en: {
         translation: {
-            WELCOME_MSG: `Welcome, you can ask The Empire about planets, people. and species. You can ask for help or you can start learning now about the people, species. and planets of the galaxy`,
+            WELCOME_MSG: `Welcome, you can ask Wookiepedia about planets, people. and species. You can ask for help or you can start learning now about the people, species. and planets of the galaxy`,
             HELP_MSG: `You can find details about anyone in the Galaxy. For example: Ask the empire to find person named Luke Skywalker. <break time="1s"/> You can find details about the different species that inhabit the galaxy. For example: Ask the empire to find details about the Hutt species. <break time="1s"/> Lastly, you can also find planetary information. For example: Ask the empire to find data for Tatooine`,
             GOODBYE_MSG: `May the force be with you!`,
             REFLECTOR_MSG: `You just triggered {{intentName}}`,
