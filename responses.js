eeffe642aeed4c8854a86e505d2f153bd86e28d6
Edward Leoni <edwardleoni@users.noreply.github.com>
2020-04-08 19:25:39 +1200

    Remove the empire references

--- responses.js
+++ responses.js
@@ -2,7 +2,7 @@ module.exports = {
     en: {
         translation: {
             WELCOME_MSG: `Welcome, you can ask Wookiepedia about planets, people. and species. You can ask for help or you can start learning now about the people, species. and planets of the galaxy`,
-            HELP_MSG: `You can find details about anyone in the Galaxy. For example: Ask the empire to find person named Luke Skywalker. <break time="1s"/> You can find details about the different species that inhabit the galaxy. For example: Ask the empire to find details about the Hutt species. <break time="1s"/> Lastly, you can also find planetary information. For example: Ask the empire to find data for Tatooine`,
+            HELP_MSG: `You can find details about anyone in the Galaxy. For example: Ask the wookie pedia to find person named Luke Skywalker. <break time="1s"/> You can find details about the different species that inhabit the galaxy. For example: Ask wookie pedia to find details about the Hutt species. <break time="1s"/> Lastly, you can also find planetary information. For example: Ask wookie pedia to find data for Tatooine`,
             GOODBYE_MSG: `May the force be with you!`,
             REFLECTOR_MSG: `You just triggered {{intentName}}`,
             ERROR_MSG: `Understand you I could not. Please repeat. Or ask for help you could try.`,
